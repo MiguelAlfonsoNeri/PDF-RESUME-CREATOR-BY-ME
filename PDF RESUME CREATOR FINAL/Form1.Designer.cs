f98ac8b40f3bba8b59a9a43d1c9c5fbc94d4aa23
unknown <migzberneri@gmail.com>
2022-07-15 12:51:45 +0800

    Just polish the project modify the design and everything

--- PDF RESUME CREATOR FINAL/Form1.Designer.cs
+++ PDF RESUME CREATOR FINAL/Form1.Designer.cs
@@ -33,18 +33,22 @@
             // 
             // BtnGenerate
             // 
-            this.BtnGenerate.Location = new System.Drawing.Point(334, 208);
+            this.BtnGenerate.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.BtnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGenerate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BtnGenerate.Location = new System.Drawing.Point(201, 203);
             this.BtnGenerate.Name = "BtnGenerate";
-            this.BtnGenerate.Size = new System.Drawing.Size(113, 38);
+            this.BtnGenerate.Size = new System.Drawing.Size(388, 38);
             this.BtnGenerate.TabIndex = 0;
-            this.BtnGenerate.Text = "GENERATE";
-            this.BtnGenerate.UseVisualStyleBackColor = true;
+            this.BtnGenerate.Text = "GENERATE JSON FILE TO PDF FILE";
+            this.BtnGenerate.UseVisualStyleBackColor = false;
             this.BtnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.BtnGenerate);
             this.Name = "Form1";
