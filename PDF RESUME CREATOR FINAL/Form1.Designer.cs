c217119a5dc23655a89e7995392b267ea9835211
unknown <migzberneri@gmail.com>
2022-07-15 09:08:27 +0800

    downloaded libraries for pdf writer

--- PDF RESUME CREATOR FINAL/Form1.Designer.cs
+++ PDF RESUME CREATOR FINAL/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnGenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnGenerate
+            // 
+            this.BtnGenerate.Location = new System.Drawing.Point(334, 208);
+            this.BtnGenerate.Name = "BtnGenerate";
+            this.BtnGenerate.Size = new System.Drawing.Size(113, 38);
+            this.BtnGenerate.TabIndex = 0;
+            this.BtnGenerate.Text = "GENERATE";
+            this.BtnGenerate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.BtnGenerate);
+            this.Name = "Form1";
+            this.Text = "Pdf Resume Generator";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnGenerate;
     }
 }
 
